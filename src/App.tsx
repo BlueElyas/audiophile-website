8aee087a43371ab5f0641bd513ae63ea2ed50e29
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-12-04 16:02:51 +0000

    add the data for the site and assets which include pngs of the products

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { RouterProvider, Route } from "react-router-dom"
+// import { RouterProvider, Route } from "react-router-dom"
 
 function App() {
   return (
     <>    
-    <RouterProvider>
-      <Route>
       <h1 className="text-3xl font-bold underline shadow-md">
         Hello world!
       </h1>
-      </Route>
-      </RouterProvider>
     </>
 
   )
