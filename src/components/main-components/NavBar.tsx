a81de139f71cdfa10a22f9d06cdfd60406064824
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-12-06 19:00:14 +0000

    add navlinks to different pages on navbar

--- src/components/main-components/NavBar.tsx
+++ src/components/main-components/NavBar.tsx
@@ -3,7 +3,10 @@ import { NavLink } from "react-router-dom";
 export function NavBar() {
     return(
         <>
-            <nav>
+            <nav className="flex gap-5">
+                <NavLink to="/headphones">Headphones</NavLink>
+                <NavLink to="/speakers">Speakers</NavLink>
+                <NavLink to="/earphones">Earphones</NavLink>
             </nav>
         </>
     )
