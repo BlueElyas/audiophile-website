de50ee62f2600d421de17dd87417fe9ac1486da4
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-12-07 14:52:32 +0000

    add text to home component and add routing for it in the app component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Layout } from "./components/main-components/Layout"
 import { Headphones } from "./pages/Headphones"
 import { Speakers } from "./pages/Speakers"
 import { Earphones } from "./pages/Earphones"
+import { Home } from "./pages/Home"
 
 function App() {
   return (
     <>    
       <Routes>
         <Route path="/" element={<Layout/>}>
+          <Route index element={<Home/>}/>
           <Route path="/headphones" element={<Headphones/>}/>
           <Route path="/speakers" element={<Speakers/>}/>
           <Route path="/earphones" element={<Earphones/>}/>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,16 @@
 export function Home() {
     return(
         <>
-            Hello
+            <div className="">
+                <img src="" alt="" />
+                <div className="">
+                    <h4></h4>
+                    <h1>XX99 Mark II HeadphoneS</h1>
+                    <p>Experience natural, lifelike audio and exceptional build quality 
+                        made for the passionate music enthusiast.</p>
+                    <button>See Product</button>
+                </div>
+            </div>
         </>
     )
 }
