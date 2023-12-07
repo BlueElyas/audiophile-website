fa1c833fea5a4dac0b24c4ab39fc220524cf7896
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-12-07 22:07:45 +0000

    create a reusable product select component

---
+++ src/components/reusable-components/ProductSelection.tsx
@@ -0,0 +1,19 @@
+import { SeeProductButton } from "./SeeProductButton"
+
+export function ProductSelection(data: any[]) {
+   return data.map(data => {
+        return(
+            <div className="flex flex-col justify-center items-center text-center mx-6 gap-8 my-16">
+                <img 
+                    src={`src/data${data.categoryImage.mobile.slice(1)}`} 
+                    alt={`picture of ${data.name}`} 
+                    className='rounded-lg'
+                    />
+                {data.new && <p className='uppercase tracking-widest text-red-600'>New product</p>}
+                <h1 className='text-4xl font-bold uppercase'>{data.name}</h1>
+                <p>{data.description}</p>
+                <SeeProductButton style="bg-[#D87D4A] text-white"/>
+            </div>
+        )
+    })
+}
