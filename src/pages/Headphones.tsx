45d7a121a28a5c6516cf2c9eb1bf0ad64bbc59d6
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-12-07 22:04:57 +0000

    create the headphones section for the headphones section

--- src/pages/Headphones.tsx
+++ src/pages/Headphones.tsx
@@ -1,7 +1,30 @@
+import { AboutSection } from '../components/reusable-components/AboutSection'
+import { SeeProductButton } from '../components/reusable-components/SeeProductButton'
+import data from '../data/data.json'
+
 export function Headphones() {
+    const headphoneData = data.filter(data => data.category === 'headphones')
+    console.log(headphoneData)
+    const mappedHeadphoneData = headphoneData.map(data => {
+        return(
+            <div className="flex flex-col justify-center items-center text-center mx-6 gap-8 my-16">
+                <img 
+                    src={`src/data${data.categoryImage.mobile.slice(1)}`} 
+                    alt={`picture of ${data.name}`} 
+                    className='rounded-lg'
+                    />
+                {data.new && <p className='uppercase tracking-widest text-red-600'>New product</p>}
+                <h1 className='text-4xl font-bold uppercase'>{data.name}</h1>
+                <p>{data.description}</p>
+                <SeeProductButton style="bg-[#D87D4A] text-white"/>
+            </div>
+        )
+    })
     return(
         <>
-            <h1>Headphones</h1>
+            <h1 className='bg-black text-white text-center py-8 uppercase text-3xl font-bold'>Headphones</h1>
+            {mappedHeadphoneData}
+            <AboutSection/>
         </>
     )
 }
