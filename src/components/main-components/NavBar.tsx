dbecd8ca2e1a78dfea712abb44fbcc1076caadee
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-12-07 14:50:47 +0000

    move the code from the navbar tsx to a reusable tsx file

--- src/components/main-components/NavBar.tsx
+++ src/components/main-components/NavBar.tsx
@@ -1,39 +1,10 @@
-import { NavLink } from "react-router-dom";
+import { mappedImageObj } from "../reusable-components/ProductNavLinks";
 export function NavBar() {
-    const imageObj :  {name: string, image: any}[] = [
-        {
-            name: "headphones",
-            image:  "src/data/assets/shared/desktop/image-category-thumbnail-headphones.png"
-        },
-        {
-            name: "speakers",
-            image: "src/data/assets/shared/desktop/image-category-thumbnail-speakers.png"
-        },
-        {
-            name: "earphones",
-            image: "src/data/assets/shared/desktop/image-category-thumbnail-earphones.png"
-        }
-    ]
-
-    const mappedImageObj : JSX.Element[] = imageObj.map(data => {
-        return (
-            <NavLink 
-                to={`/${data.name}`} 
-                className="bg-gray-200 bg-opacity-60 rounded-lg relative h-40 flex flex-col justify-end items-center">
-                <img 
-                    src={data.image}
-                    alt={`pictore of a ${data.name}`} 
-                    className="w-32 h-28 absolute top-0 -translate-y-1/3"
-                    />
-                <h1 className="font-bold tracking-wide">{data.name}</h1>
-                <p className="mt-3 mb-5">Shop <span className="text-red-400">{'>'}</span></p> 
-            </NavLink>
-        )
-    })
+   
 
     return(
         <>
-            <nav className="flex gap-16 uppercase flex-col m-6 mt-16 ">
+            <nav className="flex gap-16 uppercase flex-col m-6 mt-16 sticky top-0 z-50">
                 {mappedImageObj}
             </nav>
         </>
