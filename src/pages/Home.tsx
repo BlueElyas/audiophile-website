29357c3e610920e9367f0cfcebe199c0cff642f3
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-12-07 20:58:43 +0000

    create basic layout for home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -57,9 +57,24 @@ export function Home() {
                     alt="" 
                     className="rounded-lg"/>
                 <div className="bg-gray-100 mt-4 py-8 px-6 rounded-lg">
-                    <h1 className="font-bold text-3xl mb-6">ZX7 SPEAKER</h1>
+                    <h1 className="font-bold text-3xl mb-6">YX1 EARPHONES</h1>
                     <SeeProductButton style="border-black border-[1px] font-bold"/>
                 </div>
+            </div>
+
+            <div className="px-6 text-center flex flex-col gap-8">
+                <img 
+                    src="src\data\assets\shared\mobile\image-best-gear.jpg" 
+                    alt="picture of a man with headphones looking to the left"
+                    className="rounded-lg" />
+                <h1 className="text-4xl uppercase font=bold">Bringing you the 
+                    <span className="text-[#D87D4A]"> best</span> audio gear
+                </h1>
+                <p className="opacity-60">Located at the heart of New York City, Audiophile is the premier store for 
+                    high end headphones, earphones, speakers, and audio accessories. We have a large 
+                    showroom and luxury demonstration rooms available for you to browse and experience 
+                    a wide range of our products. Stop by our store to meet some of the fantastic people 
+                    who make Audiophile the best place to buy your portable audio equipment.</p>
             </div>
         </>
     )
