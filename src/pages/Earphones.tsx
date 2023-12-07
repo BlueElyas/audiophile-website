f1cba297974bfd8dba6703f124be47f9289662bd
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-12-07 22:09:05 +0000

    create earphones page with reusable components

--- src/pages/Earphones.tsx
+++ src/pages/Earphones.tsx
@@ -1,7 +1,14 @@
+import { AboutSection } from "../components/reusable-components/AboutSection"
+import { ProductSelection } from "../components/reusable-components/ProductSelection"
+import data from "../data/data.json"
+
 export function Earphones() {
+    const earphoneData = data.filter(data => data.category === 'earphones')
     return(
         <>
-            <h1>Earphones</h1>
+            <h1 className='bg-black text-white text-center py-8 uppercase text-3xl font-bold'>Headphones</h1>
+            {ProductSelection(earphoneData)}
+            <AboutSection/>
         </>
     )
 }
