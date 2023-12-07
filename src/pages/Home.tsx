d204f3758edb06d613338224e652df8f3d21985b
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-12-07 17:38:12 +0000

    add the ZX9 speaker banner to the homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,20 +22,22 @@ export function Home() {
 
             {mappedProductNavLinks}
 
-            <div className="bg-[#D87D4A] text-white text-center relative h-[600px]">
-                <div className="absolute">
-                    <img src="src\data\assets\home\mobile\image-speaker-zx9.png" alt="picture of a speaker"/>
-                    <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
-                        <g stroke="#FFF" fill="none" fillRule="evenodd" opacity=".202">
-                            <circle cx="472" cy="472" r="235.5"/>
-                            <circle cx="472" cy="472" r="270.5"/>
-                            <circle cx="472" cy="472" r="471.5"/>
-                        </g>
-                    </svg>                
+            <div className="bg-[#D87D4A] text-white text-center h-[600px] 
+                flex flex-col mx-6 rounded-lg items-center justify-end gap-8 px-4
+                py-4 ">
+                <div className="relative pt-4 top-6">
+                    <div className="rounded-full border-white border-2 w-64 h-64 opacity-10 flex justify-center items-center">
+                        <div className="rounded-full border-white border-2 w-56 h-56"></div>
+                    </div>
+                    <img 
+                        src="src\data\assets\home\mobile\image-speaker-zx9.png" 
+                        alt="picture of speakers" 
+                        className="absolute w-30 h-40 top-16 left-16"
+                    />
                 </div>
                 <h1 className="text-5xl">ZX9 <br /> SPEAKER</h1>
                 <p>Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
-                <SeeProductButton style="bg-black"/>
+                <SeeProductButton style="bg-black mb-8"/>
             </div>
         </>
     )
