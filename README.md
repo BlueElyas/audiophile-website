1d600236baaaf2ece8f5bf2a2c8f8ecd570282f5
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-12-07 16:08:43 +0000

    update readme file

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-This project is a multipage application using React, Tailwind, Typescript and React-Router-6 
+# **AudioPhile Multi-page Application**
+## Technologies used: React, Tailwind, Typescript
+
+This project is a multipage application using React, Tailwind, Typescript and React-Router-6
+
+### What I have done
+
+1. **SVG Components**
+
+ One thing I decided to do early on was create component of svg files in order to reduce clutter in the code. This made it to reuse the svg files as well
+
+2. **Reusable Components**
+
+ In order to make life easier, I decided to create reusable components such as the button component that can be styled with a style prop. It ensure that the design is consistent throughout and I make my code DRY. I will also be adding onClick routing props to it later on.
+
+### What I struggled with
+
+**Typescript with react**
+
+1. Using typescript for te first time in a react project, I was confused about how to handle props due to the nature of typescript. This led me to find out about specific types within react (e.g., JSX.Element and FC<props>) that has enabled a greater understanding.
+
+### What I learnt
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import { mappedProductNavLinks } from "../components/reusable-components/ProductNavLinks";
 import { SeeProductButton } from "../components/reusable-components/SeeProductButton";
-import { CirclePatternIcon } from "../components/svgs/CirclePatternIcon";
 
 export function Home() {
     return(
@@ -23,14 +22,20 @@ export function Home() {
 
             {mappedProductNavLinks}
 
-            <div className="bg-[#D87D4A] text-white">
-                <div className="div">
-                    <img src="src\data\assets\home\mobile\image-speaker-zx9.png" alt="" />
-                    <CirclePatternIcon/>
+            <div className="bg-[#D87D4A] text-white text-center relative h-[600px]">
+                <div className="absolute">
+                    <img src="src\data\assets\home\mobile\image-speaker-zx9.png" alt="picture of a speaker"/>
+                    <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
+                        <g stroke="#FFF" fill="none" fillRule="evenodd" opacity=".202">
+                            <circle cx="472" cy="472" r="235.5"/>
+                            <circle cx="472" cy="472" r="270.5"/>
+                            <circle cx="472" cy="472" r="471.5"/>
+                        </g>
+                    </svg>                
                 </div>
-                <h1>ZX9 SPEAKER</h1>
+                <h1 className="text-5xl">ZX9 <br /> SPEAKER</h1>
                 <p>Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
-                <SeeProductButton style=""/>
+                <SeeProductButton style="bg-black"/>
             </div>
         </>
     )
