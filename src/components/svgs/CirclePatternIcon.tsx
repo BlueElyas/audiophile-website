32cbe29216716d615c87e95e185de94610fa2669
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-12-07 15:54:03 +0000

    create a circle pattern icon component

--- src/components/svgs/CirclePatternIcon.tsx
+++ src/components/svgs/CirclePatternIcon.tsx
@@ -1,1 +1,7 @@
-<svg width="944" height="944" xmlns="http://www.w3.org/2000/svg"><g stroke="#FFF" fill="none" fillRule="evenodd" opacity=".202"><circle cx="472" cy="472" r="235.5"/><circle cx="472" cy="472" r="270.5"/><circle cx="472" cy="472" r="471.5"/></g></svg>
+export function CirclePatternIcon() { 
+    return <svg width="944" height="944" xmlns="http://www.w3.org/2000/svg">
+                <g stroke="#FFF" fill="none" fillRule="evenodd" opacity=".202">
+                    <circle cx="472" cy="472" r="235.5"/><circle cx="472" cy="472" r="270.5"/>
+                    <circle cx="472" cy="472" r="471.5"/>
+                </g>
+            </svg>}
