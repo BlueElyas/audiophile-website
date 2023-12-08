de807bdbed70b3da04a8567d39751bf905a26946
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-12-08 11:19:40 +0000

    add an onclick prop to the button component

--- src/components/reusable-components/SeeProductButton.tsx
+++ src/components/reusable-components/SeeProductButton.tsx
@@ -2,12 +2,14 @@ import { FC } from "react"
 
 interface ButtonProps {
     style: string
+    onClick: VoidFunction
+
 }
 
-export const SeeProductButton: FC<ButtonProps> = (props) : JSX.Element => {
+export const SeeProductButton: FC<ButtonProps> = ({style, onClick = () => {}}) : JSX.Element => {
     return(
         <>
-            <button className={`py-3 px-7 rounded-sm uppercase ${props.style} `}>
+            <button className={`py-3 px-7 rounded-sm uppercase ${style} `}>
                 See Product
             </button>
         </>
