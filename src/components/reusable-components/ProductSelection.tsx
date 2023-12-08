284f122f5c9b4437e539118fd03c71885a333f8b
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-12-08 11:55:21 +0000

    remove useeffect and usestate in product selection component0
    the id is also captured in the headphones details component

--- src/components/reusable-components/ProductSelection.tsx
+++ src/components/reusable-components/ProductSelection.tsx
@@ -1,15 +1,8 @@
-import { useEffect, useState } from "react"
 import { SeeProductButton } from "./SeeProductButton"
 import { Link } from "react-router-dom"
 
 export function ProductSelection(data: any[]) {
-    const [id, setId] = useState()
-    
-
    return data.map(data => {
-        useEffect(()=>{
-            setId(data.id)
-        },[])
         return(
             <div className="flex flex-col justify-center items-center text-center mx-6 gap-8 my-16" key={data.id}>
                 <img 
@@ -20,7 +13,7 @@ export function ProductSelection(data: any[]) {
                 {data.new && <p className='uppercase tracking-widest text-red-600'>New product</p>}
                 <h1 className='text-4xl font-bold uppercase'>{data.name}</h1>
                 <p>{data.description}</p>
-                <Link to={`/headphones/${id}`}>
+                <Link to={`/headphones/${data.id}`}>
                     <SeeProductButton style="bg-[#D87D4A] text-white" handleClick={() => {}}/>
                 </Link>
             </div>
--- src/pages/HeadphoneDetails.tsx
+++ src/pages/HeadphoneDetails.tsx
@@ -1,8 +1,15 @@
 import { useParams } from "react-router-dom"
+import data from '../data/data.json'
 
 export function HeadphoneDetails() {
-    const id = useParams()
-    console.log(id)
+    const  { id }   = useParams()
+    const selectedItem = data.filter(data => {
+        if(!id) {
+            return
+        }
+        return data.id === parseInt(id)
+    })
+    console.log(selectedItem)
     return(
         <>
 
