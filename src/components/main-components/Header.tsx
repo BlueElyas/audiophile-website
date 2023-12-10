e3a77170f7c479410d3d74541d64c3a71d544438
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-12-10 15:53:04 +0000

    fix svg icon names

--- src/components/main-components/Header.tsx
+++ src/components/main-components/Header.tsx
@@ -1,5 +1,5 @@
-import { CartIcon } from "../svgs/CartIcon";
 import { HamburgerIcon } from "../svgs/HamburgerIcon";
+import { CartIcon } from "../svgs/CartIcon";
 import { Logo } from "../svgs/Logo";
 import { useState } from 'react'
 import { NavBar } from "./NavBar";
@@ -16,13 +16,14 @@ export function Header()  {
         <>
             <div className="bg-black text-white flex justify-between px-4 py-7 items-center z-40 relative">
                 <div onClick={handleClick} className="cursor-pointer">
-                    <CartIcon/>
+                <HamburgerIcon/>
                 </div> 
                 <NavLink to="/">
                     <Logo/>
                 </NavLink>
                 <NavLink to="/cart">
-                    <HamburgerIcon/>
+                <CartIcon/>
+                    
                 </NavLink>
             </div>
             <div className="">
