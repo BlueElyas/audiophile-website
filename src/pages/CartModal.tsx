0794c6f265235681449c99185348b8bda204abb9
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-12-10 21:09:09 +0000

    create a variable holding current items in cart with their details in the modal component

--- src/pages/CartModal.tsx
+++ src/pages/CartModal.tsx
@@ -1,7 +1,24 @@
+import { fetchProductDetails } from "../utilities/fetchProductDetails"
+
 export function CartModal() {
     const cartItems : string | any = (localStorage.getItem('cart'))
     const parsedCartItems = JSON.parse(cartItems)
-    console.log(parsedCartItems)
+    // console.log(parsedCartItems)
+
+    const cartItemDetails = parsedCartItems.map((
+            item: {
+            slug: string | undefined ;
+            quantity: number
+        }) => {
+            const details = fetchProductDetails(item.slug)
+            return {
+                ...details,
+                quantity: item.quantity
+            }
+    })
+
+    
+    console.log(cartItemDetails)
     return(
         <>
             <div className="fixed z-50 top-10 bg-white p-8 shadow-[0 5px 15px rgba(0, 0, 0, 0.5)]">
