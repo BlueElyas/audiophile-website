f35e2cbb46ab89404b2ab6a7269986a701f0d54a
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-12-12 17:36:39 +0000

    add go back to the checkout component

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,13 +1,17 @@
-import { useLocation } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom"
 
 export function Checkout() {
     const location = useLocation()
+    const navigate = useNavigate()
 
-    console.log(location.state)
+    console.log(location)
 
     return(
-        <>
-            Hello
+        <>                
+            <button onClick={() => navigate(-1)} className="mb-8 opacity-60 capitalize">Go back</button>
+            <div className="">
+                <h1></h1>
+            </div>
         </>
     )
 }
