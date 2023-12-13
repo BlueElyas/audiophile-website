9cc555af32bca7fed160222a24775b7294d5d2b8
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-12-13 22:48:17 +0000

    add yx1 earphone component and added it to home page

---
+++ src/components/reusable-components/home-components/Yx1Earphones.tsx
@@ -0,0 +1,31 @@
+import { NavLink } from "react-router-dom";
+import { SeeProductButton } from "../SeeProductButton";
+import { useWindowSize } from "../../../hooks/useWindowSize";
+
+export function Yx1Earphones() {
+    const window = useWindowSize()
+    const width = window.width
+    
+    if(!width) {
+        return
+    }
+
+    const image = width >= 768 ? "/assets/home/tablet/image-earphones-yx1.jpg"  : "/assets/home/mobile/image-earphones-yx1.jpg" 
+
+    return(
+        <div className="px-6 md:flex md:justify-center md:items-center md:gap-2">
+            <div className="md:flex-1">
+                <img 
+                    src={image} 
+                    alt="" 
+                    className="rounded-lg  md:h-96"
+                />
+            </div>
+            
+            <div className="bg-gray-100 mt-4 py-8 px-6 rounded-lg md:h-96 md:mt-0 md:flex-1 md:flex md:flex-col md:justify-center">
+                <h1 className="font-bold text-3xl mb-6">YX1 EARPHONES</h1>
+                <NavLink to="speakers/yx1-earphones"><SeeProductButton style="border-black border-[1px] font-bold"/></NavLink>
+            </div>
+        </div>
+    )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,9 @@
-import { NavLink } from "react-router-dom";
 import { AboutSection } from "../components/reusable-components/AboutSection";
 import { mappedProductNavLinks } from "../components/reusable-components/ProductNavLinks";
-import { SeeProductButton } from "../components/reusable-components/SeeProductButton";
 import { Hero } from "../components/reusable-components/home-components/Hero";
 import { Zx9Speaker } from "../components/reusable-components/home-components/Zx9Speaker";
 import { Zx7Speaker } from "../components/reusable-components/home-components/Zx7Speaker";
+import { Yx1Earphones } from "../components/reusable-components/home-components/Yx1Earphones";
 
 export function Home() {
     return(
@@ -19,16 +18,7 @@ export function Home() {
             
             <Zx7Speaker/>
 
-            <div className="px-6">
-                <img 
-                    src="\assets\home\mobile\image-earphones-yx1.jpg" 
-                    alt="" 
-                    className="rounded-lg"/>
-                <div className="bg-gray-100 mt-4 py-8 px-6 rounded-lg">
-                    <h1 className="font-bold text-3xl mb-6">YX1 EARPHONES</h1>
-                    <NavLink to="speakers/yx1-earphones"><SeeProductButton style="border-black border-[1px] font-bold"/></NavLink>
-                </div>
-            </div>
+            <Yx1Earphones />
 
             <AboutSection />
         </>
