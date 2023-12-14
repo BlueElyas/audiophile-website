afa864757fa67cf36d58abf08b839726461daa59
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-12-14 20:40:46 +0000

    add desktop responsive design to the hero component

--- src/components/reusable-components/home-components/Hero.tsx
+++ src/components/reusable-components/home-components/Hero.tsx
@@ -10,7 +10,9 @@ export function Hero() {
     }
 
     return (
-        <div className=" bg-[#191919] text-white text-center flex flex-col justify-start items-center md:h-[700px] lg:mb-64 xl:mb-16">
+        <div className=" bg-[#191919] text-white text-center flex flex-col justify-start items-center 
+                        md:h-[700px] lg:mb-64 xl:mb-16"
+        >
             <div className="-translate-y-20">
                 <img 
                     src={useWindowSize(imageObj)}
@@ -18,12 +20,17 @@ export function Hero() {
                     className=" w-full "
                 />
             </div>
-            <div className="absolute top-44 left-4 uppercase grid gap-6 mx-6 md:w-[376px] md:left-[8rem] md:top-48 lg:left-[17rem] lg:top-64">
+            <div className="absolute top-44 left-4 uppercase grid gap-6 mx-6 
+                            md:w-[376px] md:left-[8rem] md:top-48 lg:left-[17rem] lg:top-64 xl:top-48 
+                            xl:left-20 xl:text-start"
+            >
                 <h4 className=" tracking-[1em] opacity-50 shadow-sm">new product</h4>
                 <h1 className="text-4xl md:text-7xl">XX99 Mark II HeadphoneS</h1>
-                <p className="normal-case opacity-70 shadow-sm md:shadow-lg">Experience natural, lifelike audio and exceptional build quality 
+                <p className="normal-case opacity-70 shadow-sm md:shadow-lg xl:text-xl">Experience natural, lifelike audio and exceptional build quality 
                     made for the passionate music enthusiast.</p>
-                <NavLink to="headphones/xx99-mark-two-headphones"><SeeProductButton style="bg-[#D87D4A] w-2/3 place-self-center md:py-6 md:w-1/2"/></NavLink>
+                <NavLink to="headphones/xx99-mark-two-headphones">
+                    <SeeProductButton style="bg-[#D87D4A] w-2/3 place-self-center md:py-6 md:w-1/2"/>
+                </NavLink>
             </div>
         </div>
     ) 
