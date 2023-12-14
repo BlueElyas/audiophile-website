29606635ca9bb87008b1cf382427332f4fce512c
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-12-14 21:45:38 +0000

    add desktop responsive design to footer component

--- src/components/main-components/Footer.tsx
+++ src/components/main-components/Footer.tsx
@@ -4,20 +4,32 @@ import { Logo } from "../svgs/Logo";
 export function Footer() {
     return(
         <>
-            <div className="bg-black text-white flex flex-col justify-center items-center text-center gap-10 px-6 md:items-start md:text-start">
+            <div className="bg-black text-white flex flex-col justify-center items-center text-center gap-10 px-6 
+                            md:items-start md:text-start xl:px-[10rem] ">
                 <div className="border-[#D87D4A] border-2 w-28"></div>
-                <Logo/>
-                <div className="flex flex-col gap-5 md:flex-row">
-                    <NavLink to="/">HOME</NavLink>
-                    <NavLink to="/headphones">HEADPHONES</NavLink>
-                    <NavLink to="/speakers">SPEAKERS</NavLink>
-                    <NavLink to="/earphones">EARPHONES</NavLink>
-                </div>
-                <p className="opacity-50">Audiophile is an all in one stop to fulfill your audio needs. We're a small 
+
+                    <div className="flex flex-col gap-6 xl:flex-row xl:w-full xl:justify-between">
+                        <div className="">
+                            <Logo/>
+                        </div>
+
+                        <div className="flex flex-col gap-5 md:flex-row ">
+                            <NavLink to="/">HOME</NavLink>
+                            <NavLink to="/headphones">HEADPHONES</NavLink>
+                            <NavLink to="/speakers">SPEAKERS</NavLink>
+                            <NavLink to="/earphones">EARPHONES</NavLink>
+                        </div>
+                    </div>
+ 
+
+                <p className="opacity-50 md:w-1/2 ">Audiophile is an all in one stop to fulfill your audio needs. We're a small 
                     team of music lovers and sound specialists who are devoted to helping you get 
                     the most out of personal audio. Come and visit our demo facility - we’re open 7 days a week.</p>
+
                 <div className="mb-16 flex flex-col justify-center items-center gap-8 w-full md:flex-row md:justify-between">
+
                 <p className="opacity-50">Copyright 2021. All Rights Reserved</p> 
+
                     <svg xmlns="http://www.w3.org/2000/svg" width="104" height="24" viewBox="0 0 104 24" fill="none">
                         <path d="M22.675 0H1.325C0.593 0 0 0.593 0 1.325V22.676C0 23.407 0.593 24 1.325 24H12.82V14.706H9.692V11.084H12.82V8.413C12.82 5.313 14.713 3.625 17.479 3.625C18.804 3.625 19.942 3.724 20.274 3.768V7.008L18.356 7.009C16.852 7.009 16.561 7.724 16.561 8.772V11.085H20.148L19.681 14.707H16.561V24H22.677C23.407 24 24 23.407 24 22.675V1.325C24 0.593 23.407 0 22.675 0Z" fill="white"/>
                         <path fillRule="evenodd" clipRule="evenodd" d="M92 0C88.741 0 88.333 0.014 87.053 0.072C82.695 0.272 80.273 2.69 80.073 7.052C80.014 8.333 80 8.741 80 12C80 15.259 80.014 15.668 80.072 16.948C80.272 21.306 82.69 23.728 87.052 23.928C88.333 23.986 88.741 24 92 24C95.259 24 95.668 23.986 96.948 23.928C101.302 23.728 103.73 21.31 103.927 16.948C103.986 15.668 104 15.259 104 12C104 8.741 103.986 8.333 103.928 7.053C103.732 2.699 101.311 0.273 96.949 0.073C95.668 0.014 95.259 0 92 0ZM92 2.163C95.204 2.163 95.584 2.175 96.85 2.233C100.102 2.381 101.621 3.924 101.769 7.152C101.827 8.417 101.838 8.797 101.838 12.001C101.838 15.206 101.826 15.585 101.769 16.85C101.62 20.075 100.105 21.621 96.85 21.769C95.584 21.827 95.206 21.839 92 21.839C88.796 21.839 88.416 21.827 87.151 21.769C83.891 21.62 82.38 20.07 82.232 16.849C82.174 15.584 82.162 15.205 82.162 12C82.162 8.796 82.175 8.417 82.232 7.151C82.381 3.924 83.896 2.38 87.151 2.232C88.417 2.175 88.796 2.163 92 2.163ZM85.838 12C85.838 8.597 88.597 5.838 92 5.838C95.403 5.838 98.162 8.597 98.162 12C98.162 15.404 95.403 18.163 92 18.163C88.597 18.163 85.838 15.403 85.838 12ZM92 16C89.791 16 88 14.21 88 12C88 9.791 89.791 8 92 8C94.209 8 96 9.791 96 12C96 14.21 94.209 16 92 16ZM96.965 5.595C96.965 4.8 97.61 4.155 98.406 4.155C99.201 4.155 99.845 4.8 99.845 5.595C99.845 6.39 99.201 7.035 98.406 7.035C97.61 7.035 96.965 6.39 96.965 5.595Z" fill="white"/>
