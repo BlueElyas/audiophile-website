7f2df67e76e5c5e9ccafabb29131ccc5a84dfdb2
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-12-14 18:30:47 +0000

    add a div with vertical margin in the home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,6 +9,7 @@ export function Home() {
     return(
         <>
             <Hero />
+            <div className="md:my-32 lg:block lg:my-[19rem]"></div>
 
             <div className="md:flex md:justify-around ">
                 {mappedProductNavLinks}
@@ -21,6 +22,7 @@ export function Home() {
             <Yx1Earphones />
 
             <AboutSection />
+
         </>
     )
 }
