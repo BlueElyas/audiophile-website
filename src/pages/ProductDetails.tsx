f8876d6728db44bc138050ee2f92ef7ad963b61b
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-12-14 22:36:07 +0000

    add desktop responsive design to the product details page

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -41,103 +41,106 @@ export function ProductDetails() {
 
 
     return (
-        <div className="p-6">
-            <button onClick={() => navigate(`/${selectedItem.category}`)} className="mb-8 opacity-60 capitalize">
-                Go back
-            </button>
+        <>
+            <div className="p-6 xl:mx-[10rem]">
+                <button onClick={() => navigate(`/${selectedItem.category}`)} className="mb-8 opacity-60 capitalize">
+                    Go back
+                </button>
 
-            <div key={selectedItem.name} className="grid gap-8">
-                <div className="flex flex-col gap-6 md:grid md:grid-cols-2 md:gap-10">
-                    <div className="md:bg-[#F1F1F1] md:rounded-lg md:flex md:flex-col md:justify-center md:overflow-hidden">
-                        <img 
-                            src={useWindowSize(selectedItem.categoryImage)} 
-                            alt={selectedItem.name} 
-                            className="rounded-lg md:scale-150"    
-                        />
-                    </div>
-                    <div className="md:flex md:flex-col md:gap-8 md:items-start">
-                        <p className="uppercase text-red-600 tracking-[0.8em]">new product</p>
-                        <h1 className="font-bold uppercase text-4xl">{selectedItem.name}</h1>
-                        <p className="opacity-50 ">{selectedItem.description}</p>
-                        <h4 className="tracking-wider font-bold">$ {selectedItem.price.toLocaleString()}</h4>
-                        <div className="flex items-center justify-around md:gap-8">
-                            <div className="flex bg-[#F1F1F1] gap-8 py-4 px-6">
-                                <button onClick={() => decreaseCartQuantity(slug!) }>-</button>
-                                <p>{quantity >= 0 ? quantity : 0}</p> 
-                                <button onClick={() => increaseCartQuantity(slug!) }>+</button>
+                <div key={selectedItem.name} className="grid gap-8">
+                    <div className="flex flex-col gap-6 md:grid md:grid-cols-2 md:gap-10 xl:gap-24">
+                        <div className="md:bg-[#F1F1F1] md:rounded-lg md:flex md:flex-col md:justify-center md:overflow-hidden">
+                            <img 
+                                src={useWindowSize(selectedItem.categoryImage)} 
+                                alt={selectedItem.name} 
+                                className="rounded-lg md:scale-150"    
+                            />
+                        </div>
+                        <div className="md:flex md:flex-col md:gap-8 md:items-start">
+                            <p className="uppercase text-red-600 tracking-[0.8em] xl:mt-16">new product</p>
+                            <h1 className="font-bold uppercase text-4xl">{selectedItem.name}</h1>
+                            <p className="opacity-50 xl:text-xl">{selectedItem.description}</p>
+                            <h4 className="tracking-wider font-bold xl:text-2xl">$ {selectedItem.price.toLocaleString()}</h4>
+                            <div className="flex items-center justify-around md:gap-8">
+                                <div className="flex bg-[#F1F1F1] gap-8 py-4 px-6">
+                                    <button onClick={() => decreaseCartQuantity(slug!) }>-</button>
+                                    <p>{quantity >= 0 ? quantity : 0}</p> 
+                                    <button onClick={() => increaseCartQuantity(slug!) }>+</button>
+                                </div>
+                                <button 
+                                    onClick={addToCart}
+                                    className="py-4 px-6 bg-[#D87D4A] text-white xl:px-10">
+                                        ADD TO CART
+                                </button>
                             </div>
-                            <button 
-                                onClick={addToCart}
-                                className="py-4 px-6 bg-[#D87D4A] text-white">
-                                    ADD TO CART
-                            </button>
+                        </div>
+                    </div>
+
+                    <div className="xl:grid xl:grid-cols-2 xl:gap-16">
+                        <div className="md:mb-16 xl:text-lg">
+                            <h5 className="font-bold text-2xl mb-4 xl:text-4xl xl:mb-7">FEATURES</h5>
+                            <p className="opacity-70">{firstHalf}</p>
+                            <br />
+                            <p className="opacity-70">{secondHalf}</p>
+                        </div>
+
+                        <div className="md:flex md:justify-between md:mr-40 md:mb-16 xl:flex-col xl:justify-normal" >
+                            <h5 className="font-bold text-2xl mb-6 md:text-3xl">IN THE BOX</h5>
+                            <div>
+                                {selectedItem.includes.map((item: { 
+                                    quantity: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; item: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined;
+                                }, index: number) => {
+                                    return <p className="opacity-60 xl:mb-4 xl:text-lg" key={index}>
+                                        <span className="text-[#D87D4A] mr-6">{item.quantity}x</span>
+                                        {item.item}
+                                    </p>
+                                })}
+                            </div>
                         </div>
                     </div>
                 </div>
-
-                <div className="md:mb-16">
-                    <h5 className="font-bold text-2xl mb-4">FEATURES</h5>
-                    <p className="opacity-70">{firstHalf}</p>
-                    <br />
-                    <p className="opacity-70">{secondHalf}</p>
+                
+                <div className="grid gap-6 mt-8 md:grid-cols-5">
+                    <div className="md:col-span-2">
+                        <img src={useWindowSize(selectedItem.gallery.first)} alt="" className="rounded-lg mb-4 md:mb-12" />
+                        <img src={useWindowSize(selectedItem.gallery.second)} alt="" className="rounded-lg" />
+                    </div>
+                    <div className="md:col-span-3">
+                        <img src={useWindowSize(selectedItem.gallery.third)} alt="" className="rounded-lg" />
+                    </div>
                 </div>
 
-                <div className="md:flex md:justify-between md:mr-40 md:mb-16" >
-                    <h5 className="font-bold text-2xl mb-6 md:text-3xl">IN THE BOX</h5>
-                    <div>
-                        {selectedItem.includes.map((item: { 
-                            quantity: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; item: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined;
-                        }, index: number) => {
-                            return <p className="opacity-60" key={index}>
-                                <span className="text-[#D87D4A] mr-6">{item.quantity}x</span>
-                                {item.item}
-                            </p>
+                <div className="text-center text-3xl font-bold my-24">
+                    <h5 className="mb-10 md:text-4xl">YOU MAY ALSO LIKE</h5>
+                    <div className="md:grid md:grid-cols-3 md:gap-6">
+                        {selectedItem.others.map((item : any) => {
+                            const specificItem = fetchProductDetails(item.slug)
+                            if (!specificItem) return
+                            const itemCategory = specificItem.category
+                            return(
+                                <div className="flex flex-col gap-8 mb-8" key={item.name}>
+                                    <img 
+                                        src={useWindowSize(item.image)} 
+                                        alt="" 
+                                        className="rounded-lg"    
+                                    />
+                                    <h5>{item.name}</h5>
+                                    <Link to={`/${itemCategory}/${item.slug}`}>
+                                        <SeeProductButton 
+                                            style="bg-[#D87D4A] text-white text-sm tracking-wide font-normal" />
+                                    </Link>
+                                </div>
+                            )
                         })}
                     </div>
                 </div>
-            </div>
-            
-            <div className="grid gap-6 mt-8 md:grid-cols-5">
-                <div className="md:col-span-2">
-                    <img src={useWindowSize(selectedItem.gallery.first)} alt="" className="rounded-lg mb-4 md:mb-12" />
-                    <img src={useWindowSize(selectedItem.gallery.second)} alt="" className="rounded-lg" />
-                </div>
-                <div className="md:col-span-3">
-                    <img src={useWindowSize(selectedItem.gallery.third)} alt="" className="rounded-lg" />
-                </div>
-            </div>
 
-            <div className="text-center text-3xl font-bold my-24">
-                <h5 className="mb-10 md:text-4xl">YOU MAY ALSO LIKE</h5>
-                <div className="md:grid md:grid-cols-3 md:gap-6">
-                    {selectedItem.others.map((item : any) => {
-                        const specificItem = fetchProductDetails(item.slug)
-                        if (!specificItem) return
-                        const itemCategory = specificItem.category
-                        return(
-                            <div className="flex flex-col gap-8 mb-8" key={item.name}>
-                                <img 
-                                    src={useWindowSize(item.image)} 
-                                    alt="" 
-                                    className="rounded-lg"    
-                                />
-                                <h5>{item.name}</h5>
-                                <Link to={`/${itemCategory}/${item.slug}`}>
-                                    <SeeProductButton 
-                                        style="bg-[#D87D4A] text-white text-sm tracking-wide font-normal" />
-                                </Link>
-                            </div>
-                        )
-                    })}
+                <div className="flex">
+                    {mappedProductNavLinks}
                 </div>
+                
             </div>
-
-            <div className="flex">
-                {mappedProductNavLinks}
-            </div>
-            
             <AboutSection />
-
-        </div>
+        </>
     )
 }
