4699f8f13dae772bd15ca3bd409eca26e520e4d4
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-12-14 21:25:18 +0000

    add desktop responsive design to about section component

--- src/components/reusable-components/AboutSection.tsx
+++ src/components/reusable-components/AboutSection.tsx
@@ -8,19 +8,21 @@ export function AboutSection() {
     }
 
     return(
-        <div className="px-6 text-center flex flex-col gap-8 mt-32 mb-28 md:font-bold md:mx-4">
+        <div className="px-6 text-center flex flex-col gap-8 mt-32 mb-28 md:font-bold md:mx-4 xl:flex-row-reverse xl:items-center">
             <img 
                 src={useWindowSize(imageObj)}
                 alt="picture of a man with headphones looking to the left"
                 className="rounded-lg" />
-            <h1 className="text-4xl uppercase md:my-4">Bringing you the 
-                <span className="text-[#D87D4A]"> best</span> audio gear
-            </h1>
-            <p className="opacity-60 md:font-normal md:text-lg">Located at the heart of New York City, Audiophile is the premier store for 
-                high end headphones, earphones, speakers, and audio accessories. We have a large 
-                showroom and luxury demonstration rooms available for you to browse and experience 
-                a wide range of our products. Stop by our store to meet some of the fantastic people 
-                who make Audiophile the best place to buy your portable audio equipment.</p>
+            <div className="flex flex-col gap-6 xl:text-start">
+                <h1 className="text-4xl uppercase md:my-4 xl:text-6xl">Bringing you the 
+                    <span className="text-[#D87D4A]"> best</span> audio gear
+                </h1>
+                <p className="opacity-60 tracking-wider md:font-normal md:text-lg xl:text-xl">Located at the heart of New York City, Audiophile is the premier store for 
+                    high end headphones, earphones, speakers, and audio accessories. We have a large 
+                    showroom and luxury demonstration rooms available for you to browse and experience 
+                    a wide range of our products. Stop by our store to meet some of the fantastic people 
+                    who make Audiophile the best place to buy your portable audio equipment.</p>
+            </div>
         </div>
     )
 }
