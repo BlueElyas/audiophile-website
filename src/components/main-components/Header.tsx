52123df8429129069cb87412d6350e26169f36d2
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-12-14 20:33:27 +0000

    add destop responsive design to the header

--- src/components/main-components/Header.tsx
+++ src/components/main-components/Header.tsx
@@ -24,19 +24,29 @@ export function Header( )  {
     const windowSize = useSpecificWindowValues()
     const windowWidth = windowSize.width
 
+    if(!windowWidth){
+        return
+    }
+
+    const desktopWidth = windowWidth >= 1280 ? '' : <div onClick={handleClick}><HamburgerIcon/></div> 
     
+    const desktopNavbar = windowWidth >=1280 ? <ul className="flex gap-8 uppercase tracking-wider">
+                                                    <NavLink to="/">Home</NavLink>
+                                                    <NavLink to="/headphones">Headphones</NavLink>
+                                                    <NavLink to="/speakers">Speakers</NavLink>
+                                                    <NavLink to="/earphones">earphones</NavLink>
+                                                </ul> : ''
 
     return windowWidth && (
         <>
             <div 
                 className="bg-[#1A1A1A] text-white flex justify-between px-4 py-7 items-center z-40 relative 
-                            md:px-10 "
+                            md:px-10 xl:px-36"
             >
 
-                {windowWidth >= 768 ? <div className="md:flex md:items-center md:gap-8">
-                    <div onClick={handleClick}>
-                        <HamburgerIcon/>
-                    </div> 
+                {windowWidth >= 768 ? 
+                <div className="md:flex md:items-center md:gap-8">
+                    {desktopWidth}
 
                     <NavLink to="/" className="md:justify-self-start md:col-span-8">
                         <Logo/>
@@ -52,16 +62,22 @@ export function Header( )  {
                     </NavLink>
                 </>}
 
+                {desktopNavbar}
+
                 <div onClick={handleModalDisplay} className="m-0 p-0 md:justify-self-end">
                     <CartIcon/>   
                 </div>
             </div>
-
+            
             <div>
                 {showNav && <NavBar/>}
-            </div>  
+            </div>
+              
+            {/* This div is to display that gray underline in the header */}
+            <div className="bg-[#1A1A1A] flex items-center justify-center relative z-30 xl:px-20">
+                <div className="bg-gray-600 h-[2px] w-[90%] opacity-30"></div>
+            </div>
 
-            <div className="bg-[#1A1A1A] flex items-center justify-center relative z-30"><div className="bg-gray-600 h-[2px] w-[90%] opacity-30"></div></div>
             {modalDisplay && <CartModal />}
         </>
     )
