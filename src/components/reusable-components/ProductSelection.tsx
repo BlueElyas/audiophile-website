90501134ed3b2ff0dd5703a6b62da30e8dc8bf19
BlueElyas <143660964+BlueElyas@users.noreply.github.com>
2023-12-14 21:56:00 +0000

    add desktop responsive design to product selection items

--- src/components/reusable-components/ProductSelection.tsx
+++ src/components/reusable-components/ProductSelection.tsx
@@ -4,19 +4,34 @@ import { Link } from "react-router-dom"
 
 export function ProductSelection(data: any[]) {
    return data.map(data => {
+    let rowOrRowReverse
+    if(data.id % 2 === 0) {
+        rowOrRowReverse = 'row'
+    }else{
+        rowOrRowReverse = 'row-reverse'
+    }
+    
         return(
-            <div className="flex flex-col justify-center items-center text-center mx-6 gap-8 my-16" key={data.id}>
-                <img 
-                    src={useWindowSize(data.categoryImage)} 
-                    alt={`picture of ${data.name}`} 
-                    className='rounded-lg'
-                    />
-                {data.new && <p className='uppercase tracking-[0.8rem] text-red-600'>New product</p>}
-                <h1 className='text-4xl font-bold uppercase'>{data.name}</h1>
-                <p>{data.description}</p>
-                <Link to={`/${data.category}/${data.slug}`} state={data}>
-                    <SeeProductButton style="bg-[#D87D4A] text-white"/>
-                </Link>
+            <div 
+                className={`flex flex-col justify-center items-center text-center mx-6 gap-8 my-16 
+                            xl:flex-${rowOrRowReverse} xl:justify-between xl:mx-[10rem] `} 
+                key={data.id}
+            >
+                    <div className="xl:flex-1">
+                    <img 
+                        src={useWindowSize(data.categoryImage)} 
+                        alt={`picture of ${data.name}`} 
+                        className='rounded-lg'
+                        />
+                </div>
+                <div className="xl:flex xl:flex-col xl:gap-6 xl:items-start xl:justify-center xl:text-start xl:flex-1">
+                    {data.new && <p className='uppercase tracking-[0.8rem] text-red-600'>New product</p>}
+                    <h1 className='text-4xl font-bold uppercase lg:text-5xl'>{data.name}</h1>
+                    <p className="">{data.description}</p>
+                    <Link to={`/${data.category}/${data.slug}`} state={data}>
+                        <SeeProductButton style="bg-[#D87D4A] text-white"/>
+                    </Link>
+                </div>
             </div>
         )
     })
